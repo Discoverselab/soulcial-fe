869fd95d8e6062a64b4a9184ec71c3301ee216a1
Renzz <49543146+Renzz926@users.noreply.github.com>
2023-11-10 21:11:25 +0800

    fix

--- src/sw.js
+++ src/sw.js
@@ -9,10 +9,11 @@ self.addEventListener("notificationclick", (evt) => {
   evt.waitUntil(
     self.clients.matchAll({ type: "window" }).then((clients) => {
       console.log("🔥🔥🔥🚀 ~ file: sw.js:16 ~ clients:", clients);
-      clients.forEach((client) => {
-        // postMessage将信息发送给界面
-        client.postMessage(evt.notification.body);
-      });
+      window.open(clients[0]?.url, '_blank');
+      // clients.forEach((client) => {
+      //   // postMessage将信息发送给界面
+      //   client.postMessage(evt.notification.body);
+      // });
     })
   );
 });
@@ -22,9 +23,12 @@ self.addEventListener("push", function (event) {
   console.log("🔥🔥🔥🚀 ~ file: sw.js:49 ~ push event:", event);
   const title = "Soulcial-test";
   const options = {
-    body: event.data.json(),
+    body: event.data.text(),
     icon: "./static/img/logo_app.4dc3ab5d.png",
     badge: "./static/img/logo_app.4dc3ab5d.png",
   };
+  console.log("🔥🔥🔥🚀 ~ file: sw.js:29 ~ text:", event.data.text());
+  console.log("🔥🔥🔥🚀 ~ file: sw.js:29 ~ json:", event.data.json());
+  console.log("🔥🔥🔥🚀 ~ file: sw.js:29 ~ parse:", JSON.parse(event.data.json()));
   self.registration.showNotification(title, options);
 });
