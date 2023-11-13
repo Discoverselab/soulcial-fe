0972bfb931d9155e42d8f91754ba6a97a96c7fff
Renzz <49543146+Renzz926@users.noreply.github.com>
2023-11-13 20:06:51 +0800

    feat: Epoch page

---
+++ src/view/epoch_introduce/main.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="vsoul_introduce">
+    <div class="navigate">
+      <img @click="$router.go(-1)" class="back" src="../../assets/back.png" />
+      <div class="nav_name">
+        PUMP Epoch Rules
+      </div>
+    </div>
+    <div class="banner">
+      <img src="../../assets/img_epoch.png" alt="">
+    </div>
+    <div class="introduce">
+      <div class="title">PUMP Epoch</div>
+      <div class="content">
+        <p>Nov 14 - Nov 28</p>
+      </div>
+      <div class="title">Double vSOUL</div>
+      <div class="content" style="border: none;">
+        <p>1. PUMP SoulCast NFT, and win double vSOUL rewards.</p>
+        <p>2. Top 10 Pumpers on leaderboard share $2100 rewards pool.</p>
+        <p class="sec"> · 1st: 800$</p>
+        <p class="sec"> · 2nd: 400$</p>
+        <p class="sec"> · 3rd: 200$</p>
+        <p class="sec"> · 4th-10th: 100$</p>
+      </div>
+
+      <div class="title">Lucky 3-in-a-row</div>
+      <div class="content">
+        <p>If you win 3 Pump in a row, you can receive a full refund in the form of ETH.</p>
+      </div>
+
+      <div class="title">Get OG Badge</div>
+      <div class="content">
+        <p>1. Participants in PUMP in the Epoch will be rewarded OG Badge. </p>
+        <p>2. OG Badge holders have early access to future features, exclusive content, and more. </p>
+      </div>
+    </div>
+    <div @click="$router.push('/vsoul_introduce')" class="btn">Learn more about vSOUL</div>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.vsoul_introduce {
+  padding: 20px 24px 40px;
+
+  .back {
+    display: block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+  }
+
+  .banner {
+    margin-bottom: 30px;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .navigate {
+    height: 48px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    top: 0px;
+    z-index: 9;
+    margin-bottom: 18px;
+
+    .nav_name {
+      justify-content: center;
+      flex: 1;
+      font-family: "Inter";
+      font-style: normal;
+      font-weight: 700;
+      font-size: 24px;
+      color: #000;
+    }
+  }
+
+  .introduce {
+    .title {
+      font-size: 16px;
+      font-weight: 600;
+      padding-bottom: 6px;
+    }
+
+    .content {
+      line-height: 20px;
+      border-bottom: 1px solid #dfdfce;
+      margin-bottom: 10px;
+      padding-bottom: 10px;
+    }
+  }
+
+  .btn {
+    width: 100%;
+    height: 48px;
+    background: #000;
+    border-radius: 8px;
+    color: #fff;
+    font-size: 16px;
+    font-weight: 600;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+  }
+
+  .sec {
+    text-indent: 1em;
+  }
+}
+</style>
