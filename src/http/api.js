f16f3f1afb1e3205c0ac98c4ce92e12468fbdf4d
Renzz <49543146+Renzz926@users.noreply.github.com>
2023-11-29 15:03:58 +0800

    fix

--- src/http/api.js
+++ src/http/api.js
@@ -1,73 +1,73 @@
-const api = process.env.VUE_APP_BASE_API
+export const website = process.env.VUE_APP_BASE_API
 
-export const website = api
+const commonApi = process.env.VUE_APP_BASE_API + '/pfp/api/admin'
 
 export default {
     login: {
-        login: `${api}/pfp/api/admin/login/login`,
-        checkSteamId: `${api}/pfp/api/admin/login/checkSteamId`,
-        setTwitterUserInfo: `${api}/pfp/api/admin/login/setTwitterUserInfo`,
-        twitterRedirect: `${api}/pfp/api/admin/login/twitterRedirect`,
-        cancelTwitterBind: `${api}/pfp/api/admin/home/cancelTwitterBind`,
+        login: `${commonApi}/login/login`,
+        checkSteamId: `${commonApi}/login/checkSteamId`,
+        setTwitterUserInfo: `${commonApi}/login/setTwitterUserInfo`,
+        twitterRedirect: `${commonApi}/login/twitterRedirect`,
+        cancelTwitterBind: `${commonApi}/home/cancelTwitterBind`,
     },
     nft: {
-        getNFTPage: `${api}/pfp/api/admin/explor/getNFTPage`, //NFT paging
-        getNFTDetail: `${api}/pfp/api/admin/explor/getNFTDetail`, //NFT details
-        getNFTLevel: `${api}/pfp/api/admin/nft/getNFTLevel`, //NFT grade
-        getMintPicture: `${api}/pfp/api/admin/nft/getMintPicture`, //Get pictures of casting NFT (return 6 pictures)
-        mintFreeNft: `${api}/pfp/api/admin/nft/mintFreeNft`, //Launch
-        collectNFT: `${api}/pfp/api/admin/explor/collectNFT`, //Buy NFT
-        listNFTApprove: `${api}/pfp/api/admin/market/listNFT`, //Buy NFT
-        cancelListNFT: `${api}/pfp/api/admin/home/cancelListNFT`, //取消出售
-        collectNFTOnline: `${api}/pfp/api/admin/explor/collectNFTOnline`, //购买
-        getNFTHistory: `${api}/pfp/api/admin/explor/getNFTHistory`, //NFT History
-        getNFTPickInfo: `${api}/pfp/api/admin/pick/getNFTPickInfo`, //getNFTPickInfo
-        pickNFT: `${api}/pfp/api/admin/pick/pickNFT`, //getNFTPickInfo
-        cutAndUpload: `${api}/pfp/api/admin/AIGC/cutAndUpload`, //getNFTPickInfo
-        getLastMintPictur: `${api}/pfp/api/admin/nft/getLastMintPictur`, //getNFTPickInfo
-        prePickNFT: `${api}/pfp/api/admin/pick/prePickNFT`, //getNFTPickInfo
-        getActivePage: `${api}/pfp/api/admin/explor/getActivePage` // 获取平台动态分页
+        getNFTPage: `${commonApi}/explor/getNFTPage`, //NFT paging
+        getNFTDetail: `${commonApi}/explor/getNFTDetail`, //NFT details
+        getNFTLevel: `${commonApi}/nft/getNFTLevel`, //NFT grade
+        getMintPicture: `${commonApi}/nft/getMintPicture`, //Get pictures of casting NFT (return 6 pictures)
+        mintFreeNft: `${commonApi}/nft/mintFreeNft`, //Launch
+        collectNFT: `${commonApi}/explor/collectNFT`, //Buy NFT
+        listNFTApprove: `${commonApi}/market/listNFT`, //Buy NFT
+        cancelListNFT: `${commonApi}/home/cancelListNFT`, //取消出售
+        collectNFTOnline: `${commonApi}/explor/collectNFTOnline`, //购买
+        getNFTHistory: `${commonApi}/explor/getNFTHistory`, //NFT History
+        getNFTPickInfo: `${commonApi}/pick/getNFTPickInfo`, //getNFTPickInfo
+        pickNFT: `${commonApi}/pick/pickNFT`, //getNFTPickInfo
+        cutAndUpload: `${commonApi}/AIGC/cutAndUpload`, //getNFTPickInfo
+        getLastMintPictur: `${commonApi}/nft/getLastMintPictur`, //getNFTPickInfo
+        prePickNFT: `${commonApi}/pick/prePickNFT`, //getNFTPickInfo
+        getActivePage: `${commonApi}/explor/getActivePage` // 获取平台动态分页
     },
     infor: {
-        useInviteCode: `${api}/pfp/api/admin/login/useInviteCode`,  // 使用邀请码
-        getInviteCodeList: `${api}/pfp/api/admin/home/getInviteCodes`,  //获取用户的邀请码列表
-        pickByInviteCode: `${api}/pfp/api/admin/home/pickByInviteCode`, // 使用邀请码pick
-        addConnect: `${api}/pfp/api/admin/connect/subConnect`, // 发起加好友请求
-        getConnectList: `${api}/pfp/api/admin/connect/list` , //获取好友请求列表
-        fetchConfirmConnect: `${api}/pfp/api/admin/connect/confirm` , //确认好友请求
-        fetchCancelConnect: `${api}/pfp/api/admin/connect/cancel` , //取消好友请求
-        getUserInfo: `${api}/pfp/api/admin/home/getUserInfo`, //Get user information
-        getMintedNFTPage: `${api}/pfp/api/admin/home/getMintedNFTPage`, //My Launch list
-        getCollectNFTPage: `${api}/pfp/api/admin/home/getCollectNFTPage`, //I bought a list.
-        setUserStreamId: `${api}/pfp/api/admin/home/setUserStreamId`, //setUserStreamId
-        updateUserScore: `${api}/pfp/api/admin/home/updateUserScore`, //setUserStreamId
-        followUser: `${api}/pfp/api/admin/home/followUser`, //setUserStreamId
-        getFollowers: `${api}/pfp/api/admin/home/getFollowers`, //被关注列表
-        getFollowing: `${api}/pfp/api/admin/home/getFollowing`, //关注列表
-        setUserInfo: `${api}/pfp/api/admin/home/setUserInfo`, //设置用户信息
-        getTagsList: `${api}/pfp/api/admin/home/getTagsList`, //获取用户标签选项list（Tags）
-        setUserTags: `${api}/pfp/api/admin/home/setUserTags`, //设置用户标签(1到12,多个用逗号隔开)
-        getPicksNFTPage: `${api}/pfp/api/admin/home/getPicksNFTPage`,
-        addWallectHistory: `${api}/pfp/api/admin/wallect/addWallectHistory`,
-        getWallectHistory: `${api}/pfp/api/admin/wallect/getWallectHistory`,
-        getVSoulBalance: `${api}/pfp/api/admin/wallect/getVSoulBalance`,
-        getVSoulHistory: `${api}/pfp/api/admin/wallect/getVSoulHistory`,
-        getNFTMood: `${api}/pfp/api/admin/nft/getNFTMood`, //Mood下拉
-        getNFTColor: `${api}/pfp/api/admin/nft/getNFTColor`, //Color下拉
-        getNFTPersonality: `${api}/pfp/api/admin/nft/getNFTPersonality`, //角色下拉
-        getFansRank: `${api}/pfp/api/admin/home/getFansRank`, //获取粉丝排行榜
-        pictureUpload: `${api}/pfp/api/admin/AIGC/pictureUpload`, //上传图片
-        getSearchList: `${api}/pfp/api/admin/explor/indexSearch`,  // 首页搜索
-        getVSoulRank: `${api}/pfp/api/admin/wallect/getVSoulRank` //vSOUL排行榜
+        useInviteCode: `${commonApi}/login/useInviteCode`,  // 使用邀请码
+        getInviteCodeList: `${commonApi}/home/getInviteCodes`,  //获取用户的邀请码列表
+        pickByInviteCode: `${commonApi}/home/pickByInviteCode`, // 使用邀请码pick
+        addConnect: `${commonApi}/connect/subConnect`, // 发起加好友请求
+        getConnectList: `${commonApi}/connect/list` , //获取好友请求列表
+        fetchConfirmConnect: `${commonApi}/connect/confirm` , //确认好友请求
+        fetchCancelConnect: `${commonApi}/connect/cancel` , //取消好友请求
+        getUserInfo: `${commonApi}/home/getUserInfo`, //Get user information
+        getMintedNFTPage: `${commonApi}/home/getMintedNFTPage`, //My Launch list
+        getCollectNFTPage: `${commonApi}/home/getCollectNFTPage`, //I bought a list.
+        setUserStreamId: `${commonApi}/home/setUserStreamId`, //setUserStreamId
+        updateUserScore: `${commonApi}/home/updateUserScore`, //setUserStreamId
+        followUser: `${commonApi}/home/followUser`, //setUserStreamId
+        getFollowers: `${commonApi}/home/getFollowers`, //被关注列表
+        getFollowing: `${commonApi}/home/getFollowing`, //关注列表
+        setUserInfo: `${commonApi}/home/setUserInfo`, //设置用户信息
+        getTagsList: `${commonApi}/home/getTagsList`, //获取用户标签选项list（Tags）
+        setUserTags: `${commonApi}/home/setUserTags`, //设置用户标签(1到12,多个用逗号隔开)
+        getPicksNFTPage: `${commonApi}/home/getPicksNFTPage`,
+        addWallectHistory: `${commonApi}/wallect/addWallectHistory`,
+        getWallectHistory: `${commonApi}/wallect/getWallectHistory`,
+        getVSoulBalance: `${commonApi}/wallect/getVSoulBalance`,
+        getVSoulHistory: `${commonApi}/wallect/getVSoulHistory`,
+        getNFTMood: `${commonApi}/nft/getNFTMood`, //Mood下拉
+        getNFTColor: `${commonApi}/nft/getNFTColor`, //Color下拉
+        getNFTPersonality: `${commonApi}/nft/getNFTPersonality`, //角色下拉
+        getFansRank: `${commonApi}/home/getFansRank`, //获取粉丝排行榜
+        pictureUpload: `${commonApi}/AIGC/pictureUpload`, //上传图片
+        getSearchList: `${commonApi}/explor/indexSearch`,  // 首页搜索
+        getVSoulRank: `${commonApi}/wallect/getVSoulRank` //vSOUL排行榜
     },
     chat: {
-        getUserMessage: `${api}/pfp/api/admin/message/getUserMessage`,
-        getRewardInfo: `${api}/pfp/api/admin/message/getRewardInfo`,
-        getChatDetail: `${api}/pfp/api/admin/message/chat/detail`,
-        getChatList: `${api}/pfp/api/admin/message/chat/list`,
-        fetchUpload: `${api}/pfp/api/admin/message/chat/upload`,
+        getUserMessage: `${commonApi}/message/getUserMessage`,
+        getRewardInfo: `${commonApi}/message/getRewardInfo`,
+        getChatDetail: `${commonApi}/message/chat/detail`,
+        getChatList: `${commonApi}/message/chat/list`,
+        fetchUpload: `${commonApi}/message/chat/upload`,
     },
     pwa: {
-        fetchSubscribe: `${api}/pfp/api/notification/subscribe`,
+        fetchSubscribe: `${website}/pfp/api/notification/subscribe`,
     }
 }
