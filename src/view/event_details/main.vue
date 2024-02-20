f3c5bf061ba70c530c0c214fd313bcd6a18aff7f
王远峰 <1950343185@qq.com>
2024-02-20 15:36:12 +0800

    feat: 谷歌地图

--- src/view/event_details/main.vue
+++ src/view/event_details/main.vue
@@ -16,11 +16,67 @@
         <img class="banner" src="@/assets/eventActive1.png" alt />
         <div class="detail">
             <h1 class="title">HongKong Web3 Festival</h1>
-            <p class="time">Apr 7 at 10:00 (UTC+8)</p>
-            <p class="desc">HKCEC HALL3FG</p>
+            <p class="time fw500">Apr 7 at 10:00 (UTC+8)</p>
+            <p class="desc fw500">HKCEC HALL3FG</p>
         </div>
+        <!-- 谷歌地图 -->
+        <div class="map">
+            <div class="mapHeader">
+                <div class="desc fw500">
+                    Hong Kong Convention and Exhibition Centre,
+                    Expo Drive, Wan Chai, Hong Kong
+                </div>
+                <div class="copy">
+                    <img src="@/assets/copyWhite.png" alt="">
+                </div>
+            </div>
+
+            <div id="mapBox"></div>
+
+        </div>
+        <!-- info -->
+        <div class="info">
+            <div class="title">
+                Info
+            </div>
+            <div class="fw500 detail">
+                The inaugural Web3 Festival, co-hosted by Wanxiang Blockchain Labs and HashKey Group and partnering with
+                OKX, will take place on April 12-15, 2023 in Hong Kong Convention and Exhibition Center (HKCEC). This
+                four-day event will see over 300 industry speakers, over 100 up-and-coming Web3 projects, esteemed venture
+                capitalist entities, and representatives from the Hong Kong government.
+            </div>
+        </div>
+        <!-- 按钮 -->
+        <div class="footerBtn">
+            <div class="btn">
+                <div class="earn_diamond">
+                    <img src="@/assets/diamond.png" alt />
+                    <span>Earn</span>
+                </div>Check in and claim airdrop
+            </div>
+        </div>
+        <TabBar ref="tabbar"></TabBar>
     </div>
 </template>
+<script>
+import TabBar from '@/components/TabBar.vue'
+import methods from './src/methods'
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: methods,
+    created() {
+        this.initMap()
+    },
+    components: {
+        TabBar,
+    }
+}
+</script>
 <style lang="scss">
 @import './sass/style.scss';
 </style>
---
+++ src/view/event_details/src/methods.js
@@ -0,0 +1,32 @@
+import { Loader } from '@googlemaps/js-api-loader' //引入
+const loader = new Loader({
+  apiKey: 'AIzaSyAgiV_tJwsbjj3twMIIQZ87f6Sz3SHWBg8', //之前的key
+  version: 'weekly', //版本
+  libraries: ['places']
+})
+
+export default {
+  initMap() {
+    let center = { lat: 40.7128, lng: -74.006 } //地图中心店
+    const mapOptions = {
+      center: center,
+      zoom: 12,
+      disableDefaultUI: true
+    }
+
+    loader
+      .load()
+      .then(google => {
+        const map = new google.maps.Map(document.getElementById('mapBox'), mapOptions)
+        // 在地图上添加标记
+        new google.maps.Marker({
+          position: center,
+          map: map
+        })
+      })
+      .catch(e => {
+        // do something
+        console.log(e)
+      })
+  }
+}
